6ef4c576208bf3dbd8708b192bc125e1a3b50317
Johann C <125331996+joehann-9s@users.noreply.github.com>
2023-05-16 00:59:54 -0500

    chore: ReminderDate added

--- api/models/Task.go
+++ api/models/Task.go
@@ -11,22 +11,22 @@ const (
 type TaskState int
 const (
 	Inbox TaskState = iota
-	Timer
+	Reminder
 	Hotbed
 	Main
 	Done
 	Archived
 )
 
-
 type Task struct {
-	gorm.Model
-	Title       string
-	Description string
-	Type        TaskType
-	State       TaskState
-	Category    []string
-	UserID      uint
-	SubTasks    []Task `gorm:"foreignKey:ParentTaskID"`
-	ParentTask  *Task  `gorm:"foreignKey:ParentTaskID"`
+    gorm.Model
+    Title         string
+    Description   string
+    Type          TaskType
+    State         TaskState
+    UserID        uint
+    SubTasks      []Task `gorm:"foreignKey:ParentTaskID"`
+    ParentTask    *Task  `gorm:"foreignKey:ParentTaskID"`
+    ReminderDate  *time.Time
+    Category	  []string
 }
