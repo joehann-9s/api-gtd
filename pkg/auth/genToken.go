5722aef87b9e863d8fba651ba08a02175f73f291
Johann C <joe7hann@proton.me>
2023-05-19 10:56:11 -0500

    chore: modified token return values, added ID into

--- pkg/auth/genToken.go
+++ pkg/auth/genToken.go
@@ -11,11 +11,12 @@ func GetJWTSecret() string {
 	return os.Getenv("JWT_SECRET")
 }
 
-func GenerateToken(username string) (string, error) {
+func GenerateToken(username string, ID uint) (string, error) {
 	JWT_SECRET := GetJWTSecret()
 	jwtKey := []byte(JWT_SECRET)
 
 	claims := jwt.MapClaims{
+		"ID":       ID,
 		"username": username,
 		"exp":      time.Now().Add(time.Hour * 24).Unix(), // 24 hours
 	}
