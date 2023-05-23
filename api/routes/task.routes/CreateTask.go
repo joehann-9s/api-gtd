c29bf75da874c3e51a113e04921ccc4b0bd0b5e6
Johann C <joe7hann@proton.me>
2023-05-22 21:47:25 -0500

    style: statuscode 201 added

--- api/routes/task.routes/CreateTask.go
+++ api/routes/task.routes/CreateTask.go
@@ -32,6 +32,8 @@ func CreateTask(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
 	json.NewEncoder(w).Encode(response)
 
 }
