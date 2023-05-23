85a1e34665a4f8e5cb199e41bbcf25ce161e57b8
Johann C <joe7hann@proton.me>
2023-05-22 20:52:17 -0500

    refactor: task and user routes group by modules

---
+++ api/routes/tasks.routes/GetTaskByID.go
@@ -0,0 +1,35 @@
+package routes
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/go-chi/render"
+	"github.com/gorilla/mux"
+	"github.com/joehann-9s/api-gtd/api/models"
+	"github.com/joehann-9s/api-gtd/pkg/auth"
+	"github.com/joehann-9s/api-gtd/pkg/db"
+)
+
+func GetTaskByID(w http.ResponseWriter, r *http.Request) {
+	var task models.Task
+	params := mux.Vars(r)
+	UserID := auth.GetIDFromClaims(r)
+
+	err := db.DB.Where("id = ? AND user_id = ?", params["id"], UserID).Preload("SubTasks").First(&task).Error
+	if err != nil {
+		render.Status(r, http.StatusNotFound)
+		render.JSON(w, r, map[string]string{"message": "Task not found"})
+		return
+	}
+
+	response := struct {
+		Message models.Task `json:"message"`
+	}{
+		Message: task,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+
+}
---
+++ api/routes/tasks.routes/GetllAllTasks.go
@@ -0,0 +1,32 @@
+package routes
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/go-chi/render"
+	"github.com/joehann-9s/api-gtd/api/models"
+	"github.com/joehann-9s/api-gtd/pkg/auth"
+	"github.com/joehann-9s/api-gtd/pkg/db"
+)
+
+func GetllAllTasks(w http.ResponseWriter, r *http.Request) {
+	var tasks []models.Task
+	UserID := auth.GetIDFromClaims(r)
+
+	err := db.DB.Where("user_id = ?", UserID).Preload("SubTasks").Find(&tasks).Error
+	if err != nil {
+		render.Status(r, http.StatusNotFound)
+		render.JSON(w, r, map[string]string{"message": "None tasks for this user"})
+		return
+	}
+	response := struct {
+		Message []models.Task `json:"message"`
+	}{
+		Message: tasks,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+
+}
